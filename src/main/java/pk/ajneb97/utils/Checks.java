9356ae5ef934b669ff1ebd08b5f9b0297f0e1710
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2023-07-23 03:29:27 -0400

    Removed unused method, small code cleanup.

--- src/main/java/pk/ajneb97/utils/Checks.java
+++ src/main/java/pk/ajneb97/utils/Checks.java
@@ -2,6 +2,7 @@ package pk.ajneb97.utils;
 
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import pk.ajneb97.PlayerKits;
@@ -14,9 +15,11 @@ public class Checks {
         String prefix = messages.getString("prefix");
         String mensaje = prefix + config.getString("Messages.materialNameError");
 
+
+        ConfigurationSection section = config.getConfigurationSection("Config.Inventory");
         //Check config.yml
-        if (config.contains("Config.Inventory")) {
-            for (String key : config.getConfigurationSection("Config.Inventory").getKeys(false)) {
+        if (section != null) {
+            for (String key : section.getKeys(false)) {
                 if (!comprobarMaterial(config.getString("Config.Inventory." + key + ".id"), jugador, mensaje)) {
                     return false;
                 }
@@ -28,28 +31,20 @@ public class Checks {
 
     @SuppressWarnings({"deprecation", "unused"})
     public static boolean comprobarMaterial(String key, CommandSender jugador, String mensaje) {
-        try {
-            if (key.contains(":")) {
-                String[] idsplit = key.split(":");
-                String stringDataValue = idsplit[1];
-                short DataValue = Short.valueOf(stringDataValue);
-                Material mat = Material.getMaterial(idsplit[0].toUpperCase());
-                ItemStack item = new ItemStack(mat, 1, (short) DataValue);
-            } else {
-                ItemStack item = new ItemStack(Material.getMaterial(key), 1);
-            }
 
-            return true;
-        } catch (Exception e) {
-            jugador.sendMessage(MessageUtils.getMensajeColor(mensaje.replace("%material%", key)));
-            return false;
+        // TODO: this needs a better code and check.
+        if (key.contains(":")) {
+            String[] idsplit = key.split(":");
+            String stringDataValue = idsplit[1];
+            short DataValue = Short.parseShort(stringDataValue);
+            Material mat = Material.getMaterial(idsplit[0].toUpperCase());
+            ItemStack item = new ItemStack(mat, 1, (short) DataValue);
+        } else {
+            ItemStack item = new ItemStack(Material.getMaterial(key), 1);
         }
-    }
 
+        //jugador.sendMessage(MessageUtils.getMensajeColor(mensaje.replace("%material%", key)));
 
-    public static void modificarPath(FileConfiguration config, String path, String idNueva) {
-        if (config.contains(path)) {
-            config.set(path, idNueva);
-        }
+        return true;
     }
 }
