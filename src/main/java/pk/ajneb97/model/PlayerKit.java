d58df3f3928d2ed19ae040d362cf7c654b82a6ac
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2023-08-07 03:45:11 -0400

    Added one-time-buy into PlayerKit, changed code to prevent npe if new option was added and is not in the saved config.

--- src/main/java/pk/ajneb97/model/PlayerKit.java
+++ src/main/java/pk/ajneb97/model/PlayerKit.java
@@ -12,12 +12,14 @@ public class PlayerKit implements ConfigurationSerializable {
     private long cooldown;
     private boolean oneTime;
     private boolean bought;
+    private boolean oneTimeBuy;
 
     public PlayerKit(Map<String, Object> map) {
         this.name = (String) map.get("name");
-        this.cooldown = (int) map.get("cooldown");
-        this.oneTime = (boolean) map.get("one-time");
-        this.bought = (boolean) map.get("bought");
+        this.cooldown = map.containsKey("cooldown") ? (int) map.get("cooldown") : -1;
+        this.oneTime = map.containsKey("one-time") && (boolean) map.get("one-time");
+        this.bought = map.containsKey("bought") && (boolean) map.get("bought");
+        this.oneTimeBuy = map.containsKey("one-time-buy") && (boolean) map.get("one-time-buy");
     }
 
     public PlayerKit(String name, boolean oneTime, boolean bought) {
@@ -65,6 +67,14 @@ public class PlayerKit implements ConfigurationSerializable {
         this.bought = bought;
     }
 
+    public boolean isOneTimeBuy() {
+        return oneTimeBuy;
+    }
+
+    public void setOneTimeBuy(boolean oneTimeBuy) {
+        this.oneTimeBuy = oneTimeBuy;
+    }
+
     @NotNull
     @Override
     public Map<String, Object> serialize() {
@@ -74,6 +84,7 @@ public class PlayerKit implements ConfigurationSerializable {
         map.put("cooldown", cooldown);
         map.put("bought", bought);
         map.put("one-time", oneTime);
+        map.put("one-time-buy", oneTimeBuy);
 
         return map;
     }
